7e3923377279b70b1f95b9b8ab49123078c9efdd
ritaOmondi <cherylritaomondi@gmail.com>
2024-08-20 16:38:16 +0000

    basic caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,55 +1,17 @@
-#!/usr/bin/env python3
-"""
-This module implements a simple caching system using a dictionary.
-
-This module provides a basic implementation of a cache. The cache is a dictionary
-that stores key-value pairs. The `get` method retrieves a value from the cache
-if it exists. Otherwise, it calls the provided function to calculate the value
-and stores it in the cache before returning it.
-"""
-
+#!/usr/bin/python3
+""" Python caching systems """
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """
-    A simple caching system using a dictionary.
-
-    This class inherits from `BaseCaching` and provides a basic implementation
-    of a cache using a dictionary. It allows storing and retrieving values
-    based on keys.
-    """
-
+    """ Dict caching system """
     def put(self, key, item):
-        """
-        Add an item in the cache.
-
-        If the key already exists in the cache, it is overwritten. If the cache
-        is full, the least recently used item is evicted to make space for the
-        new item.
-
-        Args:
-            key (any): The key to store.
-            item (any): The value to store.
-        """
-        if key is not None and item is not None:
+        """ Add an item in the cache """
+        if key is None or item is None:
+            pass
+        else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """
-        Get an item by key.
-
-        If the key exists in the cache, the corresponding value is returned.
-        Otherwise, `None` is returned.
-
-        Args:
-            key (any): The key to retrieve.
-
-        Returns:
-            any: The value associated with the key, or `None` if the key is not
-            found.
-        """
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        else:
-            return None
+        """ Get an item by key """
+        return self.cache_data.get(key)
