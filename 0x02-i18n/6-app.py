26be4c288e95494bcaf87b0a6a916969e01367a6
ritaOmondi <cherylritaomondi@gmail.com>
2024-08-27 12:11:53 +0000

    styling code

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -2,7 +2,9 @@
 from flask import Flask, render_template, request, g
 from flask_babel import Babel, _
 
+
 app = Flask(__name__)
+
 
 class Config:
     """
@@ -12,9 +14,12 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
 
+
 babel = Babel(app)
+
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -22,6 +27,7 @@ users = {
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
 
 def get_user():
     """
@@ -33,6 +39,7 @@ def get_user():
         return users.get(int(user_id))
     return None
 
+
 @babel.localeselector
 def get_locale():
     """
@@ -42,17 +49,21 @@ def get_locale():
     3. Locale from request headers
     4. Default locale
     """
+
     # Check for locale in URL parameters
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
-    
+
+
     # Check for locale in user settings
     if g.user and g.user.get('locale') in app.config['LANGUAGES']:
         return g.user['locale']
-    
+
+
     # Check for locale in request headers
     return request.accept_languages.best_match(app.config['LANGUAGES']) or app.config['BABEL_DEFAULT_LOCALE']
+
 
 @app.before_request
 def before_request():
@@ -60,6 +71,7 @@ def before_request():
     Check if a user is logged in and set it globally using Flask's 'g' object.
     """
     g.user = get_user()
+
 
 @app.route('/')
 def index():
@@ -72,6 +84,7 @@ def index():
         welcome_message = _("not_logged_in")
     
     return render_template('6-index.html', welcome_message=welcome_message)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
